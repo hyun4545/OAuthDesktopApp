4d7d828ee2e07026371156b1928e3d2b67c09d90
sandy <collet7086@gmail.com>
2023-09-24 15:18:45 +0800

    update

---
+++ OAuthDesktopApp/KeycloakService.cs
@@ -0,0 +1,198 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAuthDesktopApp
+{
+    public class KeycloakService
+    {
+        // client configuration
+        const string clientID = "sandy";
+        const string clientSecret = "MGG27dWcsFQHKBAogphoHgBp0WXPuOa2";
+        const string authorizationEndpoint = "http://localhost:8080/realms/test/protocol/openid-connect/auth";
+        const string tokenEndpoint = "http://localhost:8080/realms/test/protocol/openid-connect/token";
+        const string redirectEndpoint = "http://localhost:4200";
+
+        public async Task<string> GetTokenAsync()
+        {
+            // Generates state and PKCE values.
+            string state = randomDataBase64url(32);
+
+            // Creates an HttpListener to listen for requests on that redirect URI.
+            var http = new HttpListener();
+            http.Prefixes.Add(redirectEndpoint + "/");
+            output("Listening..");
+            http.Start();
+
+            // Creates the OAuth 2.0 authorization request.
+            string authorizationRequest = string.Format("{0}?response_type=code&scope=openid%20profile&redirect_uri={1}&client_id={2}&state={3}",
+                  authorizationEndpoint,
+                  System.Uri.EscapeDataString(redirectEndpoint),
+                  clientID,
+                  state);
+
+            //wb.Navigate(authorizationRequest);
+            // Opens request in the browser.
+            System.Diagnostics.Process.Start(authorizationRequest);
+
+            // Waits for the OAuth authorization response.
+            var context = await http.GetContextAsync();
+
+            // Sends an HTTP response to the browser.
+            var response = context.Response;
+            string responseString =
+                string.Format("<html><head><meta http-equiv='refresh' content='10;url=https://google.com'></head><body>Please return to the app.</body></html>");
+            var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            var responseOutput = response.OutputStream;
+            Task responseTask = responseOutput.WriteAsync(buffer, 0, buffer.Length).ContinueWith((task) =>
+            {
+                responseOutput.Close();
+                http.Stop();
+                Console.WriteLine("HTTP server stopped.");
+            });
+
+            // Checks for errors.
+            if (context.Request.QueryString.Get("error") != null)
+            {
+                var err = String.Format("OAuth authorization error: {0}.", context.Request.QueryString.Get("error"));
+                output(err);
+                throw new Exception(err);
+            }
+            if (context.Request.QueryString.Get("code") == null
+                || context.Request.QueryString.Get("state") == null)
+            {
+                var err = "Malformed authorization response. " + context.Request.QueryString;
+                output(err);
+                throw new Exception(err);
+            }
+            // extracts the code
+            var code = context.Request.QueryString.Get("code");
+            var incoming_state = context.Request.QueryString.Get("state");
+            var session_state = context.Request.QueryString.Get("session_state");
+
+            // Compares the receieved state to the expected value, to ensure that
+            // this app made the request which resulted in authorization.
+            if (incoming_state != state)
+            {
+                var err = String.Format("Received request with invalid state ({0})", incoming_state);
+                output(err);
+                throw new Exception(err);
+            }
+            output("Authorization code: " + code);
+
+            // Starts the code exchange at the Token Endpoint.
+            return await performCodeExchangeAsync(code, session_state, redirectEndpoint);
+        }
+
+        async Task<string> performCodeExchangeAsync(string code, string session_state, string redirectURI)
+        {
+            output("Exchanging code for tokens...");
+
+            // builds the  request
+            string tokenRequestBody = string.Format("code={0}&redirect_uri={1}&client_id={2}&session_state={3}&client_secret={4}&grant_type=authorization_code",
+                code,
+                System.Uri.EscapeDataString(redirectURI),
+                clientID,
+                session_state,
+                clientSecret
+                );
+
+            // sends the request
+            HttpWebRequest tokenRequest = (HttpWebRequest)WebRequest.Create(tokenEndpoint);
+            tokenRequest.Method = "POST";
+            tokenRequest.ContentType = "application/x-www-form-urlencoded";
+            byte[] _byteVersion = Encoding.ASCII.GetBytes(tokenRequestBody);
+            tokenRequest.ContentLength = _byteVersion.Length;
+            Stream stream = tokenRequest.GetRequestStream();
+            await stream.WriteAsync(_byteVersion, 0, _byteVersion.Length);
+            stream.Close();
+
+            try
+            {
+                // gets the response
+                WebResponse tokenResponse = await tokenRequest.GetResponseAsync();
+                using (StreamReader reader = new StreamReader(tokenResponse.GetResponseStream()))
+                {
+                    // reads response body
+                    string responseText = await reader.ReadToEndAsync();
+                    output(responseText);
+
+                    // converts to dictionary
+                    Dictionary<string, string> tokenEndpointDecoded = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseText);
+
+                    string access_token = tokenEndpointDecoded["access_token"];
+                     return access_token;
+                }
+            }
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.ProtocolError)
+                {
+                    var response = ex.Response as HttpWebResponse;
+                    if (response != null)
+                    {
+                        output("HTTP: " + response.StatusCode);
+                        using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                        {
+                            // reads response body
+                            string responseText =await reader.ReadToEndAsync();
+                            output(responseText);
+                        }
+                    }
+
+                }
+                throw ex;
+            }
+        }
+
+        public Action<string> OutputEvent;
+
+        /// <summary>
+        /// Appends the given string to the on-screen log, and the debug console.
+        /// </summary>
+        /// <param name="output">string to be appended</param>
+        public void output(string output)
+        {
+            OutputEvent?.Invoke(output);
+            Console.WriteLine(output);
+        }
+
+        /// <summary>
+        /// Returns URI-safe data with a given input length.
+        /// </summary>
+        /// <param name="length">Input length (nb. output will be longer)</param>
+        /// <returns></returns>
+        public static string randomDataBase64url(uint length)
+        {
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] bytes = new byte[length];
+            rng.GetBytes(bytes);
+            return base64urlencodeNoPadding(bytes);
+        }        
+
+        /// <summary>
+        /// Base64url no-padding encodes the given input buffer.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        public static string base64urlencodeNoPadding(byte[] buffer)
+        {
+            string base64 = Convert.ToBase64String(buffer);
+
+            // Converts base64 to base64url.
+            base64 = base64.Replace("+", "-");
+            base64 = base64.Replace("/", "_");
+            // Strips padding.
+            base64 = base64.Replace("=", "");
+
+            return base64;
+        }
+    }
+}
